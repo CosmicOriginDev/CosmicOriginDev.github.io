5be447501e3d681c62b1aff05efa9f99b1d140e2
CosmicOriginDev <154019149+CosmicOriginDev@users.noreply.github.com>
2025-10-26 13:48:44 -0500

    grfg

--- cosmicorigindevwebsite/docs/cosmicorigindevwebsite/server/angular-app-manifest.mjs
+++ cosmicorigindevwebsite/docs/cosmicorigindevwebsite/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 440, hash: '358ece6bdfb85dc9b75975084fd45c234732cab3e0a2dbc4fad458e0d50ad1ba', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 953, hash: '67a1cb44572aad24eba95221c6b35374bd21ffc9f40aa9257663befeb02d6f65', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 21585, hash: '4bd4ebd39d1e0266ad1aab1adcdbc4b0215624ebee4bc4068bd8ede6d809a24a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 440, hash: '8b2f7b660aa6ea8246439deb8abe04d2a3b3f365a765e0836548e217f63a0362', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 953, hash: 'f0386f833c49b9e3920ed9cf82c7c1dcf857d5298a7efb01fc56d6c1de2cec11', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 1458, hash: '8730e61144e6bb502925400293919d2bd7057160cec522593dcc9c9db8125c68', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
--- cosmicorigindevwebsite/src/app/app.ts
+++ cosmicorigindevwebsite/src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  selector: 'project',
+  standalone: true, // Marking as standalone
+  templateUrl: './templates/project_template.html' // Path to the external HTML template
+})
+export class Project {
+  @Input() project_name!: string;
+  @Input()  project_start!: string;
+  @Input()  project_end!: string;
+  @Input()  project_description!: string;
+}
+
+@Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Project], // Importing the standalone Project component
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('cosmicorigindevwebsite');
 }
+
